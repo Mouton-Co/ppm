af25fa820f8b9110db9a0bfd68efc1f6e1f022bd
Adriaan Mouton <85870349+An0nym0usNinja@users.noreply.github.com>
2023-06-12 12:31:01 +0200

    PPM-2: added users and roles

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,26 +21,108 @@ class UserSeeder extends Seeder
             [
                 'email' => 'arouxmouton@gmail.com',
                 'name' => 'Adriaan Mouton',
-                'role' => 'admin',
+                'roles' => ['admin'],
             ],
             [
-                'email' => 'john@ppm.co.za',
+                'email' => 'john@proproject.co.za',
                 'name' => 'John Caine',
-                'role' => 'admin',
+                'roles' => ['admin'],
+            ],
+            [
+                'email' => 'ulrich@proproject.co.za',
+                'name' => 'Ulrich Spies',
+                'roles' => ['procurement'],
+            ],
+            [
+                'email' => 'hannah@proproject.co.za',
+                'name' => 'Hannah Caine',
+                'roles' => ['procurement', 'chase'],
+            ],
+            [
+                'email' => 'donovan@proproject.co.za',
+                'name' => 'Donovan Thompson',
+                'roles' => ['procurement', 'reporting'],
+            ],
+            [
+                'email' => 'riaan@proproject.co.za',
+                'name' => 'Riaan De Wet',
+                'roles' => ['design'],
+            ],
+            [
+                'email' => 'delport@proproject.co.za',
+                'name' => 'Donovan Delport',
+                'roles' => ['design'],
+            ],
+            [
+                'email' => 'ralf@proproject.co.za',
+                'name' => 'Ralf Brugers',
+                'roles' => ['design'],
+            ],
+            [
+                'email' => 'graham@proproject.co.za',
+                'name' => 'Graham Hitzeroth',
+                'roles' => ['warehouse'],
+            ],
+            [
+                'email' => 'jone@proproject.co.za',
+                'name' => 'Jone Russouw',
+                'roles' => ['warehouse'],
+            ],
+            [
+                'email' => 'danielle@proproject.co.za',
+                'name' => 'Danielle Daniels',
+                'roles' => ['warehouse'],
+            ],
+            [
+                'email' => 'nelmari.b@proproject.co.za',
+                'name' => 'Nelmari Barnard',
+                'roles' => ['reporting', 'chase'],
+            ],
+            [
+                'email' => 'qc@proproject.co.za',
+                'name' => 'David Mpoyi',
+                'roles' => ['design'],
+            ],
+            [
+                'email' => 'celor.k@proproject.co.za',
+                'name' => 'Celor Kalenda',
+                'roles' => ['design'],
+            ],
+            [
+                'email' => 'tanaka@proproject.co.za',
+                'name' => 'Tanaka Zuze',
+                'roles' => ['design'],
+            ],
+            [
+                'email' => 'robin@proproject.co.za',
+                'name' => 'Robin Burger',
+                'roles' => ['reporting'],
+            ],
+            [
+                'email' => 'fernando@proproject.co.za',
+                'name' => 'Fernando Sneddon',
+                'roles' => ['warehouse'],
             ],
         ];
 
         foreach($users as $user) {
-            $role = Role::where('role', $user['role'])->first();
+            $roles = $user['roles'];
             $user = User::create([
-                'name' => $user['name'],
-                'email' => $user['email'],
+                'name'     => $user['name'],
+                'email'    => $user['email'],
                 'password' => $password,
             ]);
-            UserRole::create([
-                'user_id' => $user->id,
-                'role_id' => $role->id,
-            ]);
+            foreach ($roles as $roleSlug) {
+                $role = Role::where('role', $roleSlug)->first();
+                if (empty($role)) {
+                    dump('ROLE NOT FOUND');
+                    dd($roleSlug);
+                }
+                UserRole::create([
+                    'user_id' => $user->id,
+                    'role_id' => $role->id,
+                ]);
+            }
         }
     }
 }
