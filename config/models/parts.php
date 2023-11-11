580305e8c4c6ed713ceee674f27686de2937e0b0
Adriaan Mouton <arouxmouton@gmail.com>
2023-11-11 15:32:20 +0200

    PPM-52: shift parts around

--- config/models/parts.php
+++ config/models/parts.php
@@ -2,23 +2,37 @@
 
 return [
     'columns' => [
-        'name'                        => 'Name',
-        'quantity'                    => 'Quantity',
-        'material'                    => 'Material',
-        'material_thickness'          => 'Material Thickness',
-        'finish'                      => 'Finish',
-        'used_in_weldment'            => 'Used In Weldment',
-        'process_type'                => 'Process Type',
-        'manufactured_or_purchased'   => 'Manufactured or Purchased',
-        'notes'                       => 'Notes',
-        'submission->submission_code' => 'Submission',
-        'po_number'                   => 'PO Number',
-        'date_stamp'                  => 'Date Stamp',
-        'status'                      => 'Status',
-    ],
-    'editable' => [
-        'po_number',
-        'date_stamp',
-        'status',
+        'po_number' => [
+            'name' => 'PO Number',
+            'editable' => true,
+        ],
+        'submission->submission_code' => [
+            'name' => 'Submission',
+        ],
+        'status' => [
+            'name' => 'Status',
+            'editable' => true,
+        ],
+        'name' => [
+            'name' => 'Name',
+        ],
+        'quantity' => [
+            'name' => 'Quantity',
+        ],
+        'material' => [
+            'name' => 'Material',
+        ],
+        'material_thickness' => [
+            'name' => 'Material Thickness',
+        ],
+        'finish' => [
+            'name' => 'Finish',
+        ],
+        'used_in_weldment' => [
+            'name' => 'Used In Weldment',
+        ],
+        'process_type' => [
+            'name' => 'Process Type',
+        ],
     ]
 ];
--- resources/views/parts/index.blade.php
+++ resources/views/parts/index.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.dashboard')
 
-@section('custom-scripts')
-    @vite(['resources/js/parts/cell-edit.js'])
-@endsection
-
 @section('dashboard-content')
     {{-- cell form curtain --}}
     <div id="cell-curtain" class="absolute w-full h-full bg-transparent z-40 hidden"></div>
@@ -29,18 +25,18 @@
             <caption class="hidden">{{ __('Parts index table') }}</caption>
             <thead>
                 <tr>
-                    @foreach (config('models.parts.columns') as $field => $column)
+                    @foreach (config('models.parts.columns') as $key => $field)
                         <th>
                             <span class="flex items-center gap-2">
-                                {{ $column }}
+                                {{ $field['name'] }}
                                 <form action="{{ route('parts.index') }}" method="GET">
                                     <input type="hidden" name="search"
                                         value="{{ request()->query('search') ?? '' }}">
-                                    <input type="hidden" name="order_by" value="{{ $field }}">
+                                    <input type="hidden" name="order_by" value="{{ $key }}">
                                     <input type="hidden" name="page" value="{{ request()->query('page') ?? 1 }}">
                                     <input type="hidden" name="order_direction"
                                         value="{{ !empty(request()->query('order_by')) &&
-                                        request()->query('order_by') == $field &&
+                                        request()->query('order_by') == $key &&
                                         request()->query('order_direction') == 'asc'
                                             ? 'desc'
                                             : 'asc' }}">
@@ -48,7 +44,7 @@
                                         <x-icon.up-arrow
                                             class="cursor-pointer h-[10px]
                                             {{ !empty(request()->query('order_by')) &&
-                                            request()->query('order_by') == $field &&
+                                            request()->query('order_by') == $key &&
                                             request()->query('order_direction') == 'asc'
                                                 ? 'rotate-180'
                                                 : '' }}" />
@@ -65,32 +61,32 @@
                 @endphp
                 @foreach ($parts as $part)
                     <tr>
-                        @foreach (config('models.parts.columns') as $field => $column)
+                        @foreach (config('models.parts.columns') as $key => $field)
                             @php
-                                $editable = in_array($field, config('models.parts.editable'));
+                                $editable = !empty($field['editable']) && $field['editable'];
                             @endphp
                             <td>
                                 <span id="cell-{{ $cell }}"
                                 class="relative w-full h-full
                                 {{ $editable ? 'cell-edit hover:bg-sky-700 cursor-text hover:shadow-inner' : '' }}
-                                {{ $field == 'date_stamp' ? 'min-w-[150px]' : '' }}">
+                                {{ $key == 'date_stamp' ? 'min-w-[150px]' : '' }}">
                                     @php
-                                        $value = str_contains($field, '->')
-                                            ? App\Http\Services\ModelService::nestedValue($part, $field)
-                                            : $part->$field;
+                                        $value = str_contains($key, '->')
+                                            ? App\Http\Services\ModelService::nestedValue($part, $key)
+                                            : $part->$key;
                                     @endphp
                                     {{ $value ?? '-' }}
 
                                     {{-- editable field form --}}
-                                    @if (in_array($field, config('models.parts.editable')))
+                                    @if ($editable)
                                         <div class="absolute top-0 left-0 w-full h-full hidden cell-form
                                         bg-dark-field"
                                         id="cell-form-{{ $cell }}">
                                             <form action="{{ route('parts.update', $part->id) }}" method="post"
                                             class="relative w-full h-full">
                                                 @csrf
-                                                <input type="{{ $field == 'date_stamp' ? 'date' : 'text' }}"
-                                                name="{{ $field }}" value="{{ $value }}"
+                                                <input type="{{ $key == 'date_stamp' ? 'date' : 'text' }}"
+                                                name="{{ $key }}" value="{{ $field['name'] }}"
                                                 class="w-full h-full bg-transparent cell-form-input">
                                             </form>
                                         </div>
