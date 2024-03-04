f00f2cce649bda3041cd1d2e14bb09440391d5cd
Adriaan Mouton <arouxmouton@gmail.com>
2024-03-05 00:14:59 +0200

    PPM-84: add ability to bulk edit parts in warehouse

---
+++ app/Http/Requests/Parts/MarkAsRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Parts;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MarkAsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'po_number' => 'required|string',
+            'mark_as' => 'required|string|in:' . implode(',', array_keys(\App\Models\Part::$markedAs)),
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'po_number.required' => 'PO Number is required',
+            'mark_as' => 'Mark As is required',
+        ];
+    }
+}
