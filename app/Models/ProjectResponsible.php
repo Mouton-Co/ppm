50e71c7a2fb4c5e0da010fe6e26558afe1f5f15a
Adriaan Mouton <arouxmouton@gmail.com>
2024-06-13 23:28:32 +0200

    PPM-148: added project responsible generic index

--- app/Models/ProjectResponsible.php
+++ app/Models/ProjectResponsible.php
@@ -9,5 +9,48 @@ class ProjectResponsible extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = ['name'];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Index table properties
+    |--------------------------------------------------------------------------
+    */
+    public static $structure = [
+        'id' => [
+            'label' => 'ID',
+            'type' => 'text',
+            'sortable' => true,
+            'filterable' => true,
+        ],
+        'name' => [
+            'label' => 'Name',
+            'type' => 'text',
+            'sortable' => true,
+            'filterable' => true,
+        ],
+        'created_at' => [
+            'label' => 'Created At',
+            'type' => 'date',
+            'sortable' => true,
+            'filterable' => true,
+        ],
+        'updated_at' => [
+            'label' => 'Updated At',
+            'type' => 'date',
+            'sortable' => true,
+            'filterable' => true,
+        ],
+    ];
+
+    public static $actions = [
+        'create' => 'Create new',
+        'edit' => 'Edit',
+        'delete' => 'Delete',
+    ];
 }
