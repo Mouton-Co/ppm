17ac7d163c5435762e12a76da1a4d28a0e8f4b28
Adriaan Mouton <arouxmouton@gmail.com>
2024-08-27 18:56:49 +0200

    PPM-153: fixed button row merge conflict

--- resources/views/components/table/procurement/button-row.blade.php
+++ resources/views/components/table/procurement/button-row.blade.php
@@ -24,51 +24,47 @@
                 class="btn-sky text-nowrap min-w-fit max-w-fit"
                 type="submit"
             >
-                @csrf
-                <button
-                    class="btn-sky text-nowrap min-w-fit max-w-fit"
-                    type="submit"
-                >
-                    {{ __('Autofill suppliers') }}
-                </button>
+                {{ __('Autofill suppliers') }}
+            </button>
 
-                @php
-                    $options = App\Models\Supplier::all()->pluck('name', 'id')->toArray();
-                @endphp
+            @php
+                $options = App\Models\Supplier::all()->pluck('name', 'id')->toArray();
+            @endphp
 
-                <label
-                    class="min-w-fit text-white"
-                    for="lc_supplier"
-                >{{ __('Lasercut Supplier') }}</label>
-                <select
-                    class="field-dark editable-cell-dropdown !w-[195px] cursor-pointer border-none bg-transparent focus:outline-none"
-                    name="lc_supplier"
-                >
-                    <option value="0">
-                        {{ __('--Please select--') }}
+            <label
+                class="min-w-fit text-white"
+                for="lc_supplier"
+            >{{ __('Lasercut Supplier') }}</label>
+            <select
+                class="field-dark editable-cell-dropdown !w-[195px] cursor-pointer border-none bg-transparent focus:outline-none"
+                name="lc_supplier"
+            >
+                <option value="0">
+                    {{ __('--Please select--') }}
+                </option>
+                @foreach ($options as $optionKey => $optionValue)
+                    <option value="{{ $optionKey }}">
+                        {{ $optionValue }}
                     </option>
-                    @foreach ($options as $optionKey => $optionValue)
-                        <option value="{{ $optionKey }}">
-                            {{ $optionValue }}
-                        </option>
-                    @endforeach
-                </select>
-                <label
-                    class="min-w-fit text-white"
-                    for="part_supplier"
-                >{{ __('Part Supplier') }}</label>
-                <select
-                    class="field-dark editable-cell-dropdown !w-[195px] cursor-pointer border-none bg-transparent focus:outline-none"
-                    name="part_supplier"
-                >
-                    <option value="0">
-                        {{ __('--Please select--') }}
+                @endforeach
+            </select>
+            <label
+                class="min-w-fit text-white"
+                for="part_supplier"
+            >{{ __('Part Supplier') }}</label>
+            <select
+                class="field-dark editable-cell-dropdown !w-[195px] cursor-pointer border-none bg-transparent focus:outline-none"
+                name="part_supplier"
+            >
+                <option value="0">
+                    {{ __('--Please select--') }}
+                </option>
+                @foreach ($options as $optionKey => $optionValue)
+                    <option value="{{ $optionKey }}">
+                        {{ $optionValue }}
                     </option>
-                    @foreach ($options as $optionKey => $optionValue)
-                        <option value="{{ $optionKey }}">
-                            {{ $optionValue }}
-                        </option>
-                    @endforeach
-                </select>
+                @endforeach
+            </select>
         </form>
+    </div>
 @endif
